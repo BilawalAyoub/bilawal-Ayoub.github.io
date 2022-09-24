f1ccab8931f6a401a709f899b95a6c62e5650284
BilawalAyoub <114291571+BilawalAyoub@users.noreply.github.com>
2022-09-24 16:52:11 +0500

    java1

---
+++ app.js
@@ -0,0 +1,16 @@
+// war abc = prompt("Enter month");
+// switch (abc){
+//     case "Jan"
+//     alert("The month is Jan")
+    
+// }
+// var day = prompt()
+
+
+
+var mon = ["Jan","Feb","Mar","Apr"]
+var i =0;
+while (i<mon.length){
+    console.log(mon[i])
+    i++
+}
